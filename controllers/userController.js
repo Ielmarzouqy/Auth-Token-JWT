86c1363794354ed61bf991eb19325ae64437208b
Ielmarzouqy <elmarzouqyimane@gmail.com>
2023-10-18 15:28:23 +0100

    login function and jwt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 
 
 
@@ -40,12 +41,30 @@ const registerUser = asyncHandler(async (req,res)=>{
           password:newUser.password,
         },
       });
-    res.json({message:" register from controller"});
-
 })
 
 const loginUser = asyncHandler(async (req,res)=>{
-    res.json({message:"login from controller"});
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).json({message:"All fields are mandatory!"});
+  }
+  const user = await User.findOne({email})
+if(user && (await bcrypt.compare(password, user.password))){
+  const accessToken = jwt.sign(
+    {
+      user: {
+        username: user.username,
+        email: user.email,
+        id: user.id,
+      },
+    },
+    process.env.ACCESS_TOKEN_SECERT,
+    { expiresIn: "10m" }
+  
+  );
+   res.status(200).json({accessToken})
+
+}
 
 })
 
